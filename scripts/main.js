1a3f3db641c6010dd8678a0b9b57ac7b66f59362
Haoran Zhang <haoranzhang@icloud.com>
2018-12-01 02:48:50 +0000

    Add Typing Text Effect
    Add Typing Text Effect

--- scripts/main.js
+++ scripts/main.js
@@ -208,3 +208,64 @@ if (container) {
     false
   );
 }
+
+class TypeText {
+  constructor(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = "";
+    this.tick();
+    this.isDeleting = false;
+  }
+
+  tick() {
+    let i = this.loopNum % this.toRotate.length;
+    let fullText = this.toRotate[i];
+
+    if (this.isDeleting) {
+      this.txt = fullText.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullText.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+    let that = this;
+    let delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) {
+      delta /= 2;
+    }
+
+    if (!this.isDeleting && this.txt === fullText) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+
+    setTimeout(() => {
+      that.tick();
+    }, delta);
+  }
+}
+
+window.onload = () => {
+  let elements = document.querySelectorAll(".typewrite");
+  for (let i of elements) {
+    let toRotate = i.getAttribute("data-type");
+    let period = i.getAttribute("data-period");
+    if (toRotate) {
+      new TypeText(i, JSON.parse(toRotate), period);
+    }
+  }
+  let css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML =
+    ".typewrite > .wrap { border-right: 0.03em solid #000; padding-right: 5px }";
+  document.body.appendChild(css);
+};
