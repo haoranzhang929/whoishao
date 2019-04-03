3e113c4963ab1c1e674475a5f62fd620342594d0
Haoran Zhang <haoranzhang@icloud.com>
2019-04-03 21:02:31 +0100

    Update projects page
    Update projects page

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,7 +1,35 @@
 import { MDCRipple } from "@material/ripple";
 
-const btns = document.querySelectorAll(".mdc-button");
+const allBtn = document.querySelector("#all");
+const appBtn = document.querySelector("#apps");
+const sitesBtn = document.querySelector("#sites");
+const otherBtn = document.querySelector("#other");
 
-for (let btn of btns) {
-  new MDCRipple(btn);
-}
+new MDCRipple(allBtn);
+new MDCRipple(appBtn);
+new MDCRipple(sitesBtn);
+new MDCRipple(otherBtn);
+
+let onClick = className => {
+  const projects = document.querySelectorAll(".project");
+  for (let projectEl of projects) {
+    if (!projectEl.classList.contains(className)) {
+      projectEl.style.display = "none";
+    } else {
+      projectEl.style.display = "";
+    }
+  }
+};
+
+allBtn.addEventListener("click", () => {
+  onClick("project");
+});
+appBtn.addEventListener("click", () => {
+  onClick("project-app");
+});
+sitesBtn.addEventListener("click", () => {
+  onClick("project-site");
+});
+otherBtn.addEventListener("click", () => {
+  onClick("project-other");
+});
--- scripts/style.js
+++ scripts/style.js
@@ -1,21 +1,16 @@
 import { MDCTopAppBar } from "@material/top-app-bar/index";
 import { MDCDrawer } from "@material/drawer";
-import { MDCMenu } from "@material/menu";
 
 const drawer = document.querySelector(".mdc-drawer");
 const appBar = document.querySelector("#app-bar");
 const mainContextDiv = document.querySelector("#main-content");
-const projectsMenu = document.querySelector(".mdc-menu");
 const menuBtn = document.querySelector(".menu-button");
 
 const menuDrawer = MDCDrawer.attachTo(drawer);
 const topAppBar = MDCTopAppBar.attachTo(appBar);
-const menu = new MDCMenu(projectsMenu);
+//const menu = new MDCMenu(projectsMenu);
 
 topAppBar.setScrollTarget(mainContextDiv);
 topAppBar.listen("MDCTopAppBar:nav", () => {
   menuDrawer.open = !menuDrawer.open;
-});
-menuBtn.addEventListener("click", () => {
-  menu.open = !menu.open;
 });
