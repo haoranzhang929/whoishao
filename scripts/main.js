e0db8126043a4afeb9cfc4dd1d0bb923bd1b842d
Haoran Zhang <haoranzhang@icloud.com>
2019-04-15 12:33:05 +0100

    small fix
    small fix

--- scripts/main.js
+++ scripts/main.js
@@ -92,7 +92,7 @@ function startThree(container, isAboutPage) {
             (container.children[1].visible = true))
           : ((container.children[0].visible = true),
             (container.children[1].visible = false));
-        navigator.vibrate(1000);
+        navigator.vibrate(500);
       }
       container.add(mesh);
       container.add(line);
